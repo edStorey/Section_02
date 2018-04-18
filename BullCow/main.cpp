5365768c10d13ef29b2925cff6b6b42f2d069fdf
edStorey <estorey90@gmail.com>
2018-04-18 20:54:31 +0100

    Lesson 27

--- BullCow/main.cpp
+++ BullCow/main.cpp
@@ -4,21 +4,36 @@
 using namespace std;
 
 void PrintIntro(int);
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
+
+string PrintBack(std::string &Guess);
 
 int main()
 {
 	//Introduce the game
 	constexpr int WORD_LENGTH = 5;
-	PrintIntro(WORD_LENGTH);
 	
-	GetGuessAndPrintBack();
-
-	GetGuessAndPrintBack();
-
+	PrintIntro(WORD_LENGTH);
+	PlayGame();
 	return 0;
 	
 }
+
+void PlayGame()
+{
+	do {
+		constexpr int NUMBER_OF_TURNS = 5;
+		for (int i = 0; i < NUMBER_OF_TURNS; i++)
+		{
+			string Guess = GetGuess();
+			PrintBack(Guess);
+		}
+		cout << endl;
+	} while (AskToPlayAgain());
+}
+	
 
 // Print introduction text to console
 void PrintIntro(int wordLength)
@@ -29,16 +44,31 @@ void PrintIntro(int wordLength)
 	return;
 }
 
-string GetGuessAndPrintBack()
+string GetGuess()
 {
 	// get a guess from the player
 	string Guess = "";
 	cout << "Enter your guess: ";
 	getline(cin, Guess);
+	return Guess;
+	
+}
 
+string PrintBack(string &Guess)
+{
 	// repeat the guess back to them
 	cout << "Your guess was " << Guess << endl;
 	cout << endl;
 	return Guess;
-	
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again? (y/n)";
+	cout << endl;
+	string Response = "";
+	getline(cin, Response);
+
+
+	return (Response[0] == 'y' || Response[0] == 'Y');
 }
